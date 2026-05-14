0bb3f4c3ff7599c0a19a6549a9c3ca5a300d0cce
Aiden <sophia.silva@estudantes.ifc.edu.br>
2026-05-14 11:36:54 -0300

    14.05.2026 11:36

--- src/core.ts
+++ src/core.ts
@@ -1,9 +1,7 @@
+// lembrar o professor do to-do no começo do código
+
 class Item {
-
-    constructor(public title: string) {
-
-    }
-
+    constructor(public title: string) { }
 }
 
 class TodoList {
@@ -12,23 +10,45 @@ class TodoList {
     private filePath: string;
 
     constructor(filePath: string) {
-
         this.filePath = filePath;
-
+        this.readListFromDisk()
     }
 
-    addItem(item: Item){ 
+    private async saveListToDisk() {
+        const file = Bun.file(this.filePath)
+        const data = JSON.stringify(this.items)
+        await file.write(data)
+    }
 
+    private async readListFromDisk() {
+        const file = Bun.file(this.filePath)
+        // const text = await file.text()
+        // const data = JSON.parse(text)
+        const data = await file.json()
+        this.items = data.map((v: any) => {
+            return new Item(v.title)
+        })
+    }
+
+    /**
+     * Isso adiciona um novo item na lista de itens
+     */
+    async addItem(item: Item) {
         this.items.push(item);
-
+        await this.saveListToDisk()
     }
-
-    removeItem(index: number) {
-        this.items.slice(index, 1);
+    /**
+     * Remove um item da lista de item pelo índice
+     */
+    async removeItem(index: number) {
+        this.items.splice(index, 1);
+        await this.saveListToDisk()
     }
-
+    /**
+     * Retorna uma cópia da lista de itens
+     */
     getItems(): Item[] {
-        return this.items;
+        return Array.from(this.items);
         // o certo seria clonar, usando [... this.items] 
         // mas não faz muita diferença em um trabalho pequeno como este,
         // de acordo com o prof
